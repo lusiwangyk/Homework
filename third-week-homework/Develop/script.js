f5e98722999139c049d33109f64801dbd548dbda
Lusi Wang <lusiwangyk@gmail.com>
2020-03-24 14:07:10 -0400

    update homework

--- third-week-homework/Develop/script.js
+++ third-week-homework/Develop/script.js
@@ -1,24 +1,18 @@
 // Assignment Code
-
-var arrayNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var arrayLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var arrayCapitalLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var arraySpecial = [];
-
-confirm("Need a new password? Click the button to generate a new one.");
-confirm("Please select which criteria to include in the password");
-
-var passwordLength = prompt("Please choose a length of at least 8 characters and no more than 128 characters");
-
-confirm("Please select character types (At lease select one type).");
-var number = prompt("How many numbers would you like in your password?");
-var letter = prompt("How many lowercase letters would you like in your password?");
-var capitalLetter = prompt("How many capital letters would you like in your passoword?");
-var speicalCha = prompt("How many special characters would you like in your password?");
-
-alert("Please click button below to generate your new password!");
-
 var generateBtn = document.querySelector("#generate");
+
+var specialCharacter = "!@#$%^&*()";
+var letter = "abcdefghijklmnopqrstuvwxyz";
+var capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number ="0123456789";
+
+var passwordLength = prompt("Please choose how many characters would you like in your new password.");
+var specialCharacterChoose = confirm ("Would you like any special character in your password?");
+var letterChoose = confirm ("Would you like any lowercase letter in your password?");
+var capitalLetterChoose = confirm ("Would you like any capital letter in your password?");
+var numberChoose = confirm ("Would you like any number in your password?");
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -31,3 +25,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var code ="";
+
+  for ( var i = 0 ; i <= passwordLength ; i++) {
+    if  (specialCharacterChoose === true && letterChoose === true  && capitalLetterChoose === true && numberChoose === true) {
+      var everything = specialCharacter.concat(letter,capitalLetter,number);
+      code = code + everything.charAt(Math.floor(Math.random() * Math.floor(everything.length-1))));
+    }
+
+    /*else if (specialCharacterChoose === true && letterChoose === true  && capitalLetterChoose === true && numberChoose === false) {
+      var part1 = specialCharacter.concat(letter,capitalLetter);
+      alert(part1.charAt(Math.floor(Math.random() * Math.floor(part1.length -1) )));
+    }*/
+  }
+  
+
+
+
+
+}
